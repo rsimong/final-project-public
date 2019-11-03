ebee66385922fa3f6ba20b89886c1794fa351842
Ricard SG <rsimon@mariabarcelona.com>
2019-11-03 23:07:40 +0100

    Config forms

--- src/app/modules/app/layouts/auth/layouts/login/login.component.ts
+++ src/app/modules/app/layouts/auth/layouts/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -8,10 +9,49 @@ import { Title } from '@angular/platform-browser';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private titleService: Title) { }
+  lastUser = {
+    user: '',
+    avatar: '',
+    detected: false
+  };
+
+  loginForm = this.fb.group({
+    user: ['', Validators.required],
+    pass: ['', Validators.required]
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private titleService: Title
+  ) { }
 
   ngOnInit() {
     this.titleService.setTitle('Simplify | Login');
+    this.checkIfExistLastUser();
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+    console.log(this.loginForm.value);
+  }
+
+  checkIfExistLastUser() {
+    const lastUser = localStorage.getItem('last_user');
+    if (lastUser) {
+      const lastUserValues = JSON.parse(lastUser);
+      const lastUserName = lastUserValues.user;
+      const lastUserAvatar = lastUserValues.avatar;
+      this.loginForm.setValue({ user: lastUserName, pass: '' });
+      this.lastUser = { user: lastUserName, avatar: lastUserAvatar, detected: true };
+      return true;
+    }
+    return false;
+  }
+
+  allowEditUserField() {
+    this.lastUser.detected = false;
   }
 
 }
