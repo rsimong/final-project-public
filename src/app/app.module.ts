9bb512f5d3bf670847105905616ec06178ad5790
Ricard SG <rsimon@mariabarcelona.com>
2019-11-04 15:29:42 +0100

    Fake Backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { ErrorInterceptor } from '@core/interceptors/error.interceptor';
+import { BasicAuthInterceptor } from '@core/interceptors/basic-auth.interceptor';
+
+import { CoreModule } from '@core/core.module';
+
+// used to create fake backend
+import { fakeBackendProvider } from '@core/_fakeBackend/fake-backend';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +20,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: BasicAuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
