68ac301a2e0f03177b0487b55052826942ad6db0
Ricard SG <rsimon@mariabarcelona.com>
2020-03-01 16:58:31 +0100

    Create a new component: Error - Developing

---
+++ src/app/shared/components/error-developing/error-developing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-error-developing',
+  templateUrl: './error-developing.component.html',
+  styleUrls: ['./error-developing.component.css']
+})
+export class ErrorDevelopingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
