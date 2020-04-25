7ea385c6c73ab92ee161d360afa9f5707fe44abe
Ricard SG <rsimon@mariabarcelona.com>
2020-04-25 13:11:55 +0200

    Update configuration component to communicate with the new API

--- src/app/modules/app/layouts/account/components/modal-application-settings/modal-application-settings.component.ts
+++ src/app/modules/app/layouts/account/components/modal-application-settings/modal-application-settings.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthenticationService } from '@core/authentication/authentication.service';
 import { ReplyUser } from '@app/shared/models/replyUser';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { UserService } from '@shared/services/user.service';
+import { ReplyUpdateUser } from '@models/replyUpdateUser';
+
 
 @Component({
   selector: 'app-modal-application-settings',
@@ -33,142 +36,89 @@ export class ModalApplicationSettingsComponent implements OnInit {
   ];
   selectedMenuItem: Number;
   profileForm: FormGroup;
-  changePassForm: FormGroup;
-  integrations = [
-    {
-      icon: {
-        tag: 'i',
-        src: 'fab fa-google'
-      },
-      name: 'Gmail',
-      description: 'It\'s a free email service provided by the company Google.',
-      brandColor: '#D44638'
-    },
-    {
-      icon: {
-        tag: 'i',
-        src: 'fab fa-facebook-f'
-      },
-      name: 'Facebook',
-      description: 'It\'s online social media and social networking service.',
-      brandColor: '#1778F2'
-    },
-    {
-      icon: {
-        tag: 'i',
-        src: 'fab fa-slack'
-      },
-      name: 'Slack',
-      description: 'It\'s a cloud-based instant messaging platform.',
-      brandColor: '#CF0E5B'
-    },
-    {
-      icon: {
-        tag: 'i',
-        src: 'fab fa-windows'
-      },
-      name: 'Outlook',
-      description: 'It\'s a suite of webmail, contacts, tasks, and calendaring services.',
-      brandColor: '#0072C6'
-    },
-    {
-      icon: {
-        tag: 'i',
-        src: 'fab fa-whatsapp'
-      },
-      name: 'WhatsApp Messenger',
-      description: 'It\'s an instant messaging application for smartphones, in which messages are sent and received via the Internet.',
-      brandColor: '#128c7e'
-    },
-    {
-      icon: {
-        tag: 'i',
-        src: 'fab fa-telegram-plane'
-      },
-      name: 'Telegram',
-      description: 'It\'s a messaging and VOIP platform. It is focused on instant messaging, sending multiple files and mass communication.',
-      brandColor: '#0088cc'
-    },
-    {
-      icon: {
-        tag: 'i',
-        src: 'fab fa-instagram'
-      },
-      name: 'Instagram',
-      description: 'It\'s an American photo and video-sharing social networking service owned by Facebook, Inc.',
-      brandColor: '#c13584'
-    },
-    {
-      icon: {
-        tag: 'i',
-        src: 'fab fa-twitter'
-      },
-      name: 'Twitter',
-      description: 'It\'s an American microblogging and social networking service on which users post and interact with messages.',
-      brandColor: '#1da1f2'
-    },
-    {
-      icon: {
-        tag: 'i',
-        src: 'fab fa-snapchat-ghost'
-      },
-      name: 'Snapchat',
-      description: 'It\'s a messaging application for smartphones with multimedia support for image, video and augmented reality filters.',
-      brandColor: 'rgb(234, 232, 61)'
-    }
-  ];
-
   userProfileEdited: boolean = false;
+  generalSettingsEdited: boolean = false;
+  previewAvatarProfile: string = null;
 
   constructor(
     private fb: FormBuilder,
+    private userService: UserService,
     private authenticationService: AuthenticationService
   ) { }
 
   ngOnInit() {
-    this.user = null;
+    this.authenticationService.getCurrentUserValue().subscribe((value: ReplyUser) => this.user = value);
     this.selectedMenuItem = 0;
 
     this.profileForm = this.fb.group({
-      avatar: [''],
-      name: [''],
-      surnames: [''],
-      email: [''],
       settings: this.fb.group({
-        language: ['es'],
         accessibility: this.fb.group({
-          fontFamily: ['roboto'],
-          fontSize: [15],
-          highContrast: [false]
-        })
-      })
-    });
-
-    this.changePassForm = this.fb.group({
-      oldPwd: [''],
-      newPwd: [''],
-      confirmNewPwd: ['']
+          fontFamily: [this.user.settings.accessibility.fontFamily || 'default'],
+          fontSize: [this.user.settings.accessibility.fontSize || 1],
+          highContrast: [this.user.settings.accessibility.highContrast || false]
+        }),
+        language: [this.user.settings.language || 'es-ES'],
+        theme: [this.user.settings.theme || 'light']
+      }),
+      avatar: [null],
+      name: [this.user.name || ''],
+      surnames: [this.user.surnames || ''],
+      email: [this.user.email || ''],
+      password: [''],
+      repeat_password: [''],
+      old_password: [''],
+      username: [this.user.username || '']
     });
 
     this.onChanges();
   }
 
   onChanges(): void {
-    const fields = ['avatar', 'name', 'surname', 'email'];
-    fields.forEach(field => {
+    const profileFields = ['avatar', 'name', 'surnames', 'email'];
+    profileFields.forEach(field => {
       this.profileForm.get(field).valueChanges.subscribe(value => {
         let edited = false;
-        for (let i = 0; i < fields.length; i++) {
-          const fieldName = fields[i];
-          const fieldValueLength = (fieldName !== field) ? this.profileForm.value[fieldName].length : value.length;
-          if (fieldValueLength > 0) {
-            edited = true;
-            break;
-          }
+        if (value.trim() !== this.user[field]) {
+          edited = true;
         }
         this.userProfileEdited = edited;
       });
     });
+
+    const settingsFields = ['password', 'repeat_password', 'old_password', 'settings.language', 'settings.theme', 'settings.accessibility.fontFamily', 'settings.accessibility.fontSize', 'settings.accessibility.highContrast'];
+    settingsFields.forEach(field => {
+      this.profileForm.get(field).valueChanges.subscribe(value => {
+        let originalValue = (field.indexOf('.') > -1) ? field.split('.').reduce((value, key, index, allValues) => {
+          return (allValues[0] === value) ? this.user[value][key] : value[key];
+        }) : this.user[field];
+        let edited = false;
+        value = (typeof value === 'string') ? value.trim() : value;
+        if (value !== originalValue) {
+          edited = true;
+        }
+        this.generalSettingsEdited = edited;
+      });
+    });
+  }
+
+  onSubmit(): void {
+    if (!this.profileForm.valid) return;
+    this.userService.updateUser(this.profileForm.value).then((value: ReplyUpdateUser) => {
+      this.authenticationService.setCurrentUser(value.user);
+      this.userProfileEdited = false;
+      this.generalSettingsEdited = false;
+    });
+  }
+
+  uploadFile(e) {
+    const file = (e.target as HTMLInputElement).files[0];
+    this.profileForm.patchValue({ avatar: file });
+    this.profileForm.get('avatar').updateValueAndValidity()
+
+    // File Preview
+    const reader = new FileReader();
+    reader.onload = () => { this.previewAvatarProfile = reader.result as string };
+    reader.readAsDataURL(file);
   }
 
   changeMenuItem(item: Number): void {
@@ -177,14 +127,6 @@ export class ModalApplicationSettingsComponent implements OnInit {
 
   closeSettingsModal(): void {
     this.changeStateSettingsModal.emit(false);
-  }
-
-  getIconByType(type: string) {
-    return this.integrations.find((api) => api.name.toLocaleLowerCase() === type).icon.src;
-  }
-
-  getColorIconByType(type: string) {
-    return this.integrations.find((api) => api.name.toLocaleLowerCase() === type).brandColor;
   }
 
 }
