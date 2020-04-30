1c2dd4d6b6079245368cc1055c9164ecfa85125d
Ricard SG <rsimon@mariabarcelona.com>
2020-04-30 16:59:53 +0200

    Style changes and icon changes

--- src/app/modules/app/layouts/account/layouts/reply/reply.component.ts
+++ src/app/modules/app/layouts/account/layouts/reply/reply.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { ApisService } from '@shared/services/apis.service';
+import { ReplyAccessToken } from '@models/replyAccessToken';
 
 @Component({
   selector: 'app-reply',
@@ -8,10 +10,51 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class ReplyComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  replyData = {};
+
+  constructor(
+    private route: ActivatedRoute,
+    private apisServices: ApisService
+  ) { }
 
   ngOnInit() {
+    this.route.queryParamMap.subscribe((params) => {
+      this.replyData = { ...params['params'] };
+      this.sendDataToServer();
+    });
+  }
 
+  sendDataToServer() {
+    const apiID = sessionStorage.getItem('APIRequested') || null;
+    const requestName = sessionStorage.getItem('request') || null;
+    const body = {
+      request: requestName,
+      reply: { ...this.replyData }
+    };
+    if (apiID) {
+      this.apisServices.requestAccessToken(apiID, body)
+        .then((resp: ReplyAccessToken) => {
+          if (resp.finish) {
+            sessionStorage.removeItem('request');
+            sessionStorage.removeItem('APIRequested');
+            window.close();
+            return;
+          }
+          if (resp.request) this.newExternalRequest(resp);
+        })
+        .catch((error) => console.debug(error));
+    }
+  }
+
+  newExternalRequest(request: ReplyAccessToken) {
+    const requestParams = request.request;
+    this.apisServices.externalRequest(requestParams.method, requestParams.uri, requestParams.headers, requestParams.body)
+      .then((data: any) => {
+        this.replyData = { ...data };
+        sessionStorage.setItem('request', request.requestType);
+        this.sendDataToServer();
+      })
+      .catch((error) => console.debug(error));
   }
 
 }
